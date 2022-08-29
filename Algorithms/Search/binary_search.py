fc703cedfe2e4507ecaac77e7900a804b0a920af
joshuadavidang <chunxiong_@outlook.sg>
2022-08-29 15:56:04 +0800

    implemented binary_search

---
+++ Algorithms/Search/binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(array, target):
+    first = 0  # first element in the array
+    last = len(array) - 1  # last element in the array
+
+    while first <= last:
+        mid_point = (first + last) // 2
+        if array[mid_point] == target:
+            # target found
+            return mid_point
+
+        elif array[mid_point] < target:
+            # ignore values on the left, iterate forward
+            first = mid_point + 1
+
+        elif array[mid_point] > target:
+            # ignore values on the right, iterate backwards
+            last = mid_point - 1
+
+    return None
+
+customer_id = [2, 3, 7, 9, 12, 13]
+find_id = binary_search(customer_id, 12)
+print(f"Index of the target is at position {find_id}")
+
