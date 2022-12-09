2bc1c63fa88fd314268f86117e029ba85ab59f98
joshuadavidang <joshuadavidang@outlook.sg>
2022-12-09 22:11:28 +0800

    Bug fixed

--- Algorithms/Searching & Sorting/bubble_sort.py
+++ Algorithms/Searching & Sorting/bubble_sort.py
@@ -1,12 +1,12 @@
-def bubble_sort(list_a):
+def bubble_sort(data):
     is_sorted = False
     while not is_sorted:
         is_sorted = True
-        for i in range(len(list_a) - 1):
-            if arr[i] > arr[i + 1]:
+        for i in range(len(data) - 1):
+            if data[i] > data[i + 1]:
                 is_sorted = False
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    return list_a
+                data[i], data[i + 1] = data[i + 1], data[i]
+    return data
 
 
 arr = [64, 34, 25, 12, 22, 11, 100, 90]
