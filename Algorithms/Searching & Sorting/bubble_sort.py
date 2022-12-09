4f1f934061c21d6f68cc3da1f8646601dd224721
joshuadavidang <joshuadavidang@outlook.sg>
2022-12-09 17:01:16 +0800

    code refactored

--- Algorithms/Searching & Sorting/bubble_sort.py
+++ Algorithms/Searching & Sorting/bubble_sort.py
@@ -1,20 +1,13 @@
-# def bubble_sort(list_a):
-#     is_sorted = False
-#     while not is_sorted:
-#         is_sorted = True
-#         for i in range(len(list_a) - 1):
-#             if arr[i] > arr[i + 1]:
-#                 is_sorted = False
-#                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
-#     return list_a
-#
-#
-# arr = [64, 34, 25, 12, 22, 11, 100, 90]
-# print(bubble_sort(arr))
+def bubble_sort(list_a):
+    is_sorted = False
+    while not is_sorted:
+        is_sorted = True
+        for i in range(len(list_a) - 1):
+            if arr[i] > arr[i + 1]:
+                is_sorted = False
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+    return list_a
 
 
-"""
-Input: nums = [2,7,11,15], target = 9
-Output: [0,1]
-Output: Because nums[0] + nums[1] == 9, we return [0, 1]
-"""
+arr = [64, 34, 25, 12, 22, 11, 100, 90]
+print(bubble_sort(arr))
